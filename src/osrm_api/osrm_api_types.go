570403082042ab12e6e273f1658bf518b565e2d9
reVInotip <grigoriy.novikov220@gmail.com>
2025-11-02 12:37:33 +0700

    Replace pgRounting to OSRM API queries

---
+++ src/osrm_api/osrm_api_types.go
@@ -0,0 +1,50 @@
+package osrmapi
+
+type OSRMResponse struct {
+    Code   string    `json:"code"`
+    Routes []Route   `json:"routes"`
+    Waypoints []Waypoint `json:"waypoints"`
+}
+
+type Route struct {
+    Distance float64   `json:"distance"` // в метрах
+    Duration float64   `json:"duration"` // в секундах
+    Geometry GeoJson    `json:"geometry"` // полилиния в формате polyline
+    Legs     []Leg     `json:"legs"`
+    Weight   float64   `json:"weight"`
+    WeightName string  `json:"weight_name"`
+}
+
+type Leg struct {
+    Distance float64   `json:"distance"`
+    Duration float64   `json:"duration"`
+    Steps    []Step    `json:"steps"`
+    Summary  string    `json:"summary"`
+}
+
+type Step struct {
+    Distance    float64   `json:"distance"`
+    Duration    float64   `json:"duration"`
+    Geometry    string    `json:"geometry"`
+    Name        string    `json:"name"`
+    Instruction string    `json:"instruction"`
+    Maneuver    Maneuver  `json:"maneuver"`
+}
+
+type Maneuver struct {
+    Location []float64 `json:"location"`
+    BearingAfter int   `json:"bearing_after"`
+    Type      string   `json:"type"`
+    Modifier  string   `json:"modifier,omitempty"`
+}
+
+type Waypoint struct {
+    Name      string    `json:"name"`
+    Location  []float64 `json:"location"`
+    Distance  float64   `json:"distance"`
+}
+
+type GeoJson struct {
+	Type string `json:"type"`
+	Coordinates [][]float64 `json:"coordinates"`
+}
--- src/routing_hooks.go
+++ src/routing_hooks.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"GruzowikiRoutesGenerator/db"
+	osrmapi "GruzowikiRoutesGenerator/osrm_api"
+	"GruzowikiRoutesGenerator/utils/dto"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
-	"GruzowikiRoutesGenerator/utils/dto"
-	"GruzowikiRoutesGenerator/db"
 )
 
-var OnCreateRouteHandler =
+var OnCreateRouteHandlerWithPGRouting =
 	func(c *fiber.Ctx) error {
 		c.Accepts("html")             // "html"
 		c.Accepts("text/html")        // "text/html"
@@ -22,7 +24,34 @@ var OnCreateRouteHandler =
 
 		pgr := c.Locals("pgr").(db.PGRoutingQueries)
 
-		routeDto, err := pgr.BuildRout(createRouteDTO)
+		routeDto, err := pgr.BuildRoute(createRouteDTO)
+		if err != nil && !routeDto.IsDtoValid() {
+			log.Error("QueryRow failed: %v\n", err)
+			return c.Status(fiber.StatusTeapot).JSON(routeDto)
+		} else if err != nil {
+			log.Error("QueryRow failed: %v\n", err)
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+
+		return c.Status(fiber.StatusOK).JSON(routeDto)
+	}
+
+var OnCreateRouteHandlerWithOSRMApi =
+	func(c *fiber.Ctx) error {
+		c.Accepts("html")             // "html"
+		c.Accepts("text/html")        // "text/html"
+		c.Accepts("json", "text")     // "json"
+		c.Accepts("application/json") // "application/json"
+
+		createRouteDTO := new(dto.CreateRouteDTO)
+
+		if err := c.BodyParser(createRouteDTO); err != nil {
+            return err
+        }
+
+		osrm := c.Locals("osrm").(osrmapi.OSRMQueries)
+
+		routeDto, err := osrm.BuildRoute(createRouteDTO)
 		if err != nil && !routeDto.IsDtoValid() {
 			log.Error("QueryRow failed: %v\n", err)
 			return c.Status(fiber.StatusTeapot).JSON(routeDto)
